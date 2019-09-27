81cd95af9ac16afa2969ffaa447733efced3966e
thanapoomp <thanapoom.isc@gmail.com>
2019-09-27 13:21:27 +0700

    Updates

---
+++ precache-manifest.4c7dd6d8a46b3906cab57b68f5ff0b5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8a8c752734cd6b7f2667b4effc3bbcf",
+    "url": "/my-react-publish/index.html"
+  },
+  {
+    "revision": "be63723adb9ed75b0d75",
+    "url": "/my-react-publish/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3183491d2132fadc39c4",
+    "url": "/my-react-publish/static/js/2.45756c7f.chunk.js"
+  },
+  {
+    "revision": "be63723adb9ed75b0d75",
+    "url": "/my-react-publish/static/js/main.b2146cd9.chunk.js"
+  },
+  {
+    "revision": "ea307bce00619221b9a6",
+    "url": "/my-react-publish/static/js/runtime-main.8262cf1e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/my-react-publish/static/media/logo.25bf045c.svg"
+  }
+]);
